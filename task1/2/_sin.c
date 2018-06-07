0788df6d86a6dcff5be1f29b5c15484e7cff67f0
Pavel Fedotov <pashka0989@yandex.ru>
2018-06-07 10:05:41 +0700

    add task1 and task2 + travis-ci

---
+++ task1/2/_sin.c
@@ -0,0 +1,6 @@
+#include <math.h>
+
+float _sin(int a)
+{
+    return sin(a);
+}
---
+++ task1/3/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(){
+    int a;
+    scanf("%i", &a);
+    printf("\na = %i\n", a);
+    return 0;
+}
