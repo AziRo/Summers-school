1a005b015f8ccb781586cf6a3bd43bd17b8b057c
Pavel Fedotov <pashka0989@yandex.ru>
2018-07-17 21:30:31 +0700

    Multiprocessing client-server

---
+++ task17/UDP/client.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+#define BUF_SIZE 256
+
+
+int main()
+{
+    struct sockaddr_in saddr;
+
+    bzero(&saddr, sizeof(saddr));
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = 5690;
+    saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int size_dest;
+    char buf[BUF_SIZE];
+
+    while(1){
+        int pid = fork();
+        if (pid == 0) {
+            size_dest = sizeof(saddr);
+            int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+            /* Подключение к основному серверу */
+            bzero(buf, BUF_SIZE);
+            strcpy(buf, "New connection.");
+            sendto(fd, buf, strlen(buf), 0, (struct sockaddr *) &saddr, size_dest);
+
+            /* Получение данных о новом сервере */
+            recvfrom(fd, buf, BUF_SIZE, 0, (struct sockaddr *) &saddr, &size_dest);
+            printf("%s\n", buf);
+
+            /* Обмен сообщениями с новым сервером */
+            bzero(buf, BUF_SIZE);
+            sprintf(buf, "Client %i: Hello server!", getpid());
+            sendto(fd, buf, BUF_SIZE, 0, (struct sockaddr *) &saddr, size_dest);
+            recvfrom(fd, buf, BUF_SIZE, 0, (struct sockaddr *) &saddr, &size_dest);
+            printf("%s\n", buf);
+
+            sleep(20);
+            close(fd);
+            exit(0);
+        } else {
+            sleep(1);
+        }
+    }
+
+    return 0;
+}
---
+++ task17/UDP/server.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+#define BUF_SIZE 256
+
+
+int main()
+{
+    int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    struct sockaddr_in caddr, saddr;
+
+    bzero(&saddr, sizeof(saddr));
+    bzero(&saddr, sizeof(caddr));
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = 5690;
+    saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    bind(fd, (struct sockaddr *) &saddr, sizeof(saddr));
+
+    char buf[BUF_SIZE];
+    int size_dest = sizeof(caddr);
+
+    while (1) {
+        /* Принимаем новые подключения */
+        recvfrom(fd, buf, BUF_SIZE, 0, (struct sockaddr *) &caddr, &size_dest);
+        printf("%s\n", buf);
+
+        /* Создаём новый сокет */
+        int new_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+        /* Отправляем сообщение клиенту и данные о endpoint */
+        bzero(buf, BUF_SIZE);
+        strcpy(buf, "You'r connected!");
+        sendto(new_fd, buf, BUF_SIZE, 0, (struct sockaddr *) &caddr, size_dest);
+
+        /* Создаём процесс обработки клиента */
+        int pid = fork();
+        if (pid == 0) {
+            size_dest = sizeof(caddr);
+            recvfrom(new_fd, buf, BUF_SIZE, 0, (struct sockaddr *) &caddr, &size_dest);
+            printf("%s\n", buf);
+            bzero(buf, BUF_SIZE);
+            sprintf(buf, "Server %i: Hello client!", getpid());
+            sendto(new_fd, buf, BUF_SIZE, 0, (struct sockaddr *) &caddr, size_dest);
+
+            sleep(20);
+            close(new_fd);
+            exit(0);
+        } else {
+            sleep(1);
+        }
+    }
+    close(fd);
+
+    return 0;
+}
