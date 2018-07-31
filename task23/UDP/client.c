8f81887f7fb339dff7fd5b25333154399181b222
Pavel Fedotov <pashka0989@yandex.ru>
2018-07-31 23:33:10 +0700

    fixed checksum

--- task23/UDP/client.c
+++ task23/UDP/client.c
@@ -14,10 +14,9 @@
 #include <net/if.h>
 
 
-
 #define MSG_SIZE 256
 #define S_PORT 8888
-#define D_PORT 9999
+#define D_PORT 1555
 
 #define IPv4 0x800
 #define CRC16 0x8005
@@ -31,47 +30,24 @@ struct udp_msg {
 } __attribute__((packed));
 
 
-uint16_t gen_crc16(const uint8_t *data, uint16_t size)
+uint16_t gen_crc16(const uint16_t *data, uint16_t size)
 {
-    uint16_t out = 0;
-    int bits_read = 0, bit_flag;
+    int i = 0;
+    uint32_t crc = 0;
 
-    if (data == NULL)
-        return 0;
+    while (i < size) {
 
-    while (size > 0) {
-        bit_flag = out >> 15;
+        crc = crc + data[i];
 
-        out <<= 1;
-        out |= (*data >> bits_read) & 1;
-
-        bits_read++;
-        if (bits_read > 7) {
-            bits_read = 0;
-            data++;
-            size--;
+        if (crc > 0xFFFF) {
+            crc -= 0x10000;
+            crc += 0x1;
         }
 
-        if (bit_flag)
-            out ^= CRC16;
+        ++i;
     }
 
-    int i;
-    for (i = 0; i < 16; ++i) {
-        bit_flag = out >> 15;
-        out <<= 1;
-        if(bit_flag)
-            out ^= CRC16;
-    }
-
-    uint16_t crc = 0;
-    i = 0x8000;
-    int j = 0x0001;
-    for (; i != 0; i >>=1, j <<= 1) {
-        if (i & out) crc |= j;
-    }
-
-    return crc;
+    return ~(crc);
 }
 
 
@@ -88,17 +64,18 @@ int main()
 
     saddr.sll_family = AF_PACKET;
     saddr.sll_halen = ETH_ALEN;
-    saddr.sll_ifindex = if_nametoindex("wlp3s0");
+    saddr.sll_ifindex = if_nametoindex("enp4s0f1");
 
     struct udp_msg msg;
     bzero(&msg, sizeof(msg));
+
+    strcpy(msg.buff, "Hello server!");
 
     /* Заполнение UDP заголовка */
     msg.udp_hdr.source = htons(S_PORT);
     msg.udp_hdr.dest = htons(D_PORT);
     msg.udp_hdr.len = htons(MSG_SIZE + 8);
-    msg.udp_hdr.check = gen_crc16((const uint8_t *)&msg, sizeof(msg));
-
+    msg.udp_hdr.check = gen_crc16((uint16_t *)&msg, sizeof(msg)/2);
 
     /* Заполнение IP заголовка */
     msg.ip_hdr.ihl = 5;
@@ -109,39 +86,37 @@ int main()
     msg.ip_hdr.frag_off = 0;
     msg.ip_hdr.ttl = 64;
     msg.ip_hdr.protocol = IPPROTO_UDP;
-    msg.ip_hdr.check = gen_crc16((const uint8_t *)&(msg.ip_hdr), sizeof(msg.ip_hdr));
-    msg.ip_hdr.saddr = inet_addr("127.0.0.1");
-    msg.ip_hdr.daddr = inet_addr("192.168.0.106");
-
+    msg.ip_hdr.saddr = inet_addr("192.168.0.60");
+    msg.ip_hdr.daddr = inet_addr("192.168.0.30");
+    msg.ip_hdr.check = gen_crc16((uint16_t*) &(msg.ip_hdr), sizeof(msg.ip_hdr)/2);
 
     /* Заполнение заголовка канального уровня */
-    msg.eth_hdr.h_source[0] = 0xc8;
-    msg.eth_hdr.h_source[1] = 0xff;
-    msg.eth_hdr.h_source[2] = 0x28;
-    msg.eth_hdr.h_source[3] = 0x40;
-    msg.eth_hdr.h_source[4] = 0x2b;
-    msg.eth_hdr.h_source[5] = 0xb9;
+    msg.eth_hdr.h_source[0] = 0x54;
+    msg.eth_hdr.h_source[1] = 0xab;
+    msg.eth_hdr.h_source[2] = 0x3a;
+    msg.eth_hdr.h_source[3] = 0x96;
+    msg.eth_hdr.h_source[4] = 0x4f;
+    msg.eth_hdr.h_source[5] = 0x32;
 
-    msg.eth_hdr.h_dest[0] = 0xf8;
-    msg.eth_hdr.h_dest[1] = 0x1a;
-    msg.eth_hdr.h_dest[2] = 0x67;
-    msg.eth_hdr.h_dest[3] = 0xc3;
-    msg.eth_hdr.h_dest[4] = 0xe5;
-    msg.eth_hdr.h_dest[5] = 0x16;
+    msg.eth_hdr.h_dest[0] = 0xb8;
+    msg.eth_hdr.h_dest[1] = 0x27;
+    msg.eth_hdr.h_dest[2] = 0xeb;
+    msg.eth_hdr.h_dest[3] = 0x20;
+    msg.eth_hdr.h_dest[4] = 0xe4;
+    msg.eth_hdr.h_dest[5] = 0x2c;
 
     msg.eth_hdr.h_proto = htons(IPv4);
 
     int size_dest = sizeof(saddr);
 
-    strcpy(msg.buff, "Hello server!");
     sendto(fd, &msg, sizeof(msg), 0, (struct sockaddr *) &saddr, size_dest);
 
     if (errno != 0) {
         printf("ERROR2: %s\n",strerror(errno));
     }
 
-    for(int i = 0; i < 5; ++i) {
-    	recvfrom(fd, &msg, MSG_SIZE + 28, 0, (struct sockaddr *) &saddr, &size_dest);
+    while(1) {
+    	recvfrom(fd, &msg, sizeof(msg), 0, (struct sockaddr *) &saddr, &size_dest);
         if (msg.udp_hdr.dest == htons(S_PORT)) {
             printf("%s\n", msg.buff);
             break;
